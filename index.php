5098c1c14c8f5de3e0c051784edf02b2bc994360
Andrea030105 <tummoloandrea05@gmail.com>
2026-02-19 13:27:52 +0100

    ex-done

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+class movie
+{
+    public $titolo;
+    public $durata = 0;
+    public $lingua;
 
+    function __construct($titolo, $lingua)
+    {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+    }
+
+    public function setDurata($durata)
+    {
+        $this->durata = $durata;
+    }
+
+    public function getDurata()
+    {
+        $this->durata;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +33,19 @@
 </head>
 
 <body>
-
+    <?php
+    $movie_1 = new movie("IO SONO LEGGENDA ", "ITALIANO");
+    $movie_2 = new movie("FAST AND FURIOS ", "ITALIANO");
+    $movie_1->setDurata("1.30 ");
+    $movie_2->setDurata("1.50 ");
+    echo $movie_1->titolo;
+    echo $movie_1->durata;
+    echo $movie_1->lingua;
+    echo "<br>";
+    echo $movie_2->titolo;
+    echo $movie_2->durata;
+    echo $movie_2->lingua;
+    ?>
 </body>
 
 </html>
